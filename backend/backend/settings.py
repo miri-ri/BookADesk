654ac60ee13c5533953317e052273575b9fcf143
xjosii <xjosiponto@gmail.com>
2022-06-04 20:06:53 -0500

    bug fixed - rename app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book_a_desk',
-    'users'
+    'users',
+    'workplace'
 ]
 
 MIDDLEWARE = [
--- backend/workplace/apps.py
+++ backend/workplace/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BookASeatConfig(AppConfig):
+class WorkplaceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'book_a_seat'
+    name = 'workplace'
