47ef1d4067a8e28a3d902df76dcfdfcc6404d5aa
Sumalvico <niklas.sumalvico@gmail.com>
2022-06-06 20:20:47 +0200

    Basic User Crud

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        return queryset
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        return queryset
