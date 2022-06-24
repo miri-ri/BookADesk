87a1c014c1473a6a0d7b2dbcadb74d246d7ccdcb
xjosii <xjosiponto@gmail.com>
2022-06-24 08:30:19 +0200

    added Room to Workplace model

--- backend/workplace/models.py
+++ backend/workplace/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+
+class Room (models.Model):
+    name = models.CharField(max_length=5)
+    
 class Workspace(models.Model):
     name = models.CharField(max_length=5)
     additionalinfo = models.CharField(max_length=150)
     is_barrier_free = models.BooleanField()
     has_computer = models.BooleanField()
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
 
