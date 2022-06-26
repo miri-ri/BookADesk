832c2cffa49a615aa8d8f9c68f1f0bce706840fc
Miriam <61996077+miri-ri@users.noreply.github.com>
2022-06-27 00:56:10 +0200

    delete backend files

--- backend/reservation/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ReservationDetail, ReservationList
-
-urlpatterns = [
-    path('reservations/',ReservationList.as_view()),
-    path('reservations/<int:pk>/', ReservationDetail.as_view())
-    ]
