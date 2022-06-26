81064c2bf8f6fad37df2041a2b0ae8cd59ff00dd
miri-ri <miriamrichter.d@gmail.com>
2022-06-26 12:44:00 +0200

    Advanced table

--- frontend/book-a-seat/src/components/reservations/Reservation.js
+++ frontend/book-a-seat/src/components/reservations/Reservation.js
@@ -53,6 +53,13 @@ let reservations = [
     seat_id: 3,
     start: "2022-06-27T11:00:00+02:00",
     duration: 6,
+  },
+  {
+    id: 5,
+    user_id: 2,
+    seat_id: 3,
+    start: "2022-06-28T12:00:00+02:00",
+    duration: 4,
   }
 ]
 
@@ -77,19 +84,34 @@ function Reservation() {
 
   var col = workplaces.length;
 
+  var selectedRoom = 0;
+
+  const seats = (
+    <>
+      <tr id="seats-row" class="div-hidden">
+          <th scope="col" class="sidebar">Seats:</th>
+          { days && days.map((day, i) => (
+            workplaces && workplaces.map((workplace, index) => <th class="cell-selected" id ={i+"_seat_"+index} onClick={(e) => buttonRoom(i+"_room_"+index)}>{workplace}</th>)
+          )) }
+      </tr>
+    </>
+  )
+
   const table = (
     <>
       <table class="table">
         <thead>
           <tr class="t-row">
-            <th scope="col" class="sidebar" rowSpan="2"></th>
-            { days && days.map(e => <th id="dynAmountSeats" class="t-head" colSpan={col}>{e}</th>) }
+            <th scope="col" class="sidebar"></th>
+            { days && days.map((e, index) => <th id={"day_"+index} class="t-head" colSpan={col}>{e}</th>) }
           </tr>
           <tr class="t-row">
-          { days && days.map(day => (
-            workplaces && workplaces.map(workplace => <th class="t-head">{workplace}</th>)
+          <th scope="col" class="sidebar">Rooms:</th>
+          { days && days.map((day, i) => (
+            workplaces && workplaces.map((workplace, index) => <th class="t-head" id ={i+"_room_"+index} onClick={(e) => buttonRoom(i+"_room_"+index)}>{workplace}</th>)
           )) }
           </tr>
+          {seats}
         </thead>
         <tbody>
           {times.map(time => (
@@ -114,6 +136,20 @@ function Reservation() {
         {myReservations}
     </>
   );
+}
+
+function buttonRoom(id) {
+  console.log(id)
+  var element = document.getElementById(id);
+  var row = document.getElementById("seats-row")
+  if(element.className==="cell-selected") {
+    element.className = "t-head"
+    row.className = "div-hidden"
+  } else {
+    element.className = "cell-selected"
+    row.className ="t-row"
+  }
+
 }
 
 var selectedSlots_ID=[]
@@ -157,16 +193,39 @@ function saveChanges(){
   var resWithoutDuplicats = selectedSlots_ID
   console.log(duplicats)*/
 
-  var test = selectedSlots_ID.map(e => [parseInt(e.split('_')[0]), parseInt(e.split('_')[1]), parseInt(e.split('_')[2])])
-  var test2 = test.map(i => {
+  var slotsInt = selectedSlots_ID.map(e => [parseInt(e.split('_')[0]), parseInt(e.split('_')[1]), parseInt(e.split('_')[2])])
+  var duplicates = slotsInt.map(i => {
     var later = [i[0], i[1], i[2]+1]
-    if(test.filter(f => later[0]==f[0] && later[1]==f[1] && later[2]==f[2]).length!=0){
+    if(slotsInt.filter(f => later[0]==f[0] && later[1]==f[1] && later[2]==f[2]).length!=0){
       console.log("earlier(i): " + i + ", later: " + later)
       return i;
     }
   })
-  console.log(test)
-  console.log(test2)
+  console.log(slotsInt)
+  console.log(duplicates)
+  // for loop über alle slots, counter für einen slot (auf platz 0 und 1), add to array[id, duration]
+  var reservations = []
+  slotsInt.map((e) => {
+      var duration = 1;
+      for(let i=0;i<duplicates.length; i++){
+        if(duplicates[i]!=undefined&&e[0]==(duplicates[i])[0]&&e[1]==(duplicates[i])[1]){
+          duplicates[i]=undefined;
+          duration++;
+        }
+      }
+      reservations.push([e, duration])
+  })
+  console.log(reservations)
+  var fil = reservations.filter(a => a[1]!=1)
+  console.log(fil)
+
+  // einzelne Slots werden zu längeren hinzugefügt 
+  // ursprüngliche Liste wird durchgegangen, wenn element[0 und 1] gleich und [2]>=res[2] und [2]<res[2]+duration
+  var allRes = slotsInt.map(e=> {
+    if(e[0]){}
+  })
+  // remove duplicates from slots
+  // add other slots as duration 1 (for loop oder map mit push funktion)
 }
 
 var diffDays = 0;
