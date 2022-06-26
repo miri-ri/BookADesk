931eab8b8687af5dd2c2130cea4dfc4048c8c8fd
xjosii <xjosiponto@gmail.com>
2022-06-27 00:52:14 +0200

    group/room crud

--- backend/workspace/serializers.py
+++ backend/workspace/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Workspace
+from .models import Group
 
 
 class WorkspaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Workspace
+        fields = ('__all__')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
         fields = ('__all__')
--- backend/workspace/views.py
+++ backend/workspace/views.py
@@ -1,11 +1,14 @@
 from os import stat
 from django.http import JsonResponse
+from .models import Group
 from .models import Workspace
+from .serializers import GroupSerializer
 from .serializers import WorkspaceSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+# Workspaces
 
 @api_view(['GET'])
 def workspace_list(request):
@@ -44,4 +47,45 @@ def workspace_delete(request, delete_id):
 
     workspace = Workspace.objects.get(id=delete_id)
     workspace.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Groups
+
+@api_view(['GET'])
+def group_list(request):
+    group = Group.objects.all()
+    serializer = GroupSerializer(group, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def group_add(request):
+    serializer = GroupSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT'])
+def group_edit(request, edit_id):
+
+    group = Group.objects.get(id=edit_id)
+
+    if request.method == "GET":
+        serializer = GroupSerializer(group)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = GroupSerializer(group, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def group_delete(request, delete_id):
+
+    group = Group.objects.get(id=delete_id)
+    group.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
